2310be08619f3d12bc371f0821c2f3dfac3b52ba
Sh1iba <zafar_ibrogimov@mail.ru>
2023-09-24 15:38:43 +0300

    1

---
+++ src/Praktik_5/Recursion.java
@@ -0,0 +1,18 @@
+package Praktik_5;
+
+public class Recursion {
+
+    public static void recursion(int N) {
+        if (N < 10) {
+            System.out.print(N + "\t");
+        } else {
+            recursion(N/10);
+            System.out.print(N % 10 + "\t");
+
+        }
+    }
+
+    public static void main (String[]args) {
+        recursion(896);
+        }
+}
